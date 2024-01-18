d8e380624bce31d3c03379fafe5172c0c0c28585
javierdlrc <javierdlrc@JAVIERDELOS6BBB>
2024-01-17 19:18:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Mission2v2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numRolls = 0;
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        numRolls = int.Parse(Console.ReadLine());
+
+        rollDice rd = new rollDice();
+
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = {numRolls}.");
+
+        rollDice rollDice = new rollDice();
+        int[] summary = rollDice.RollingOfDice(numRolls);
+
+        for (int i = 2; i <= 12; i++) 
+        { 
+            int percentage = summary[i] * 100 / numRolls;
+            Console.WriteLine($"{i}: {new string('*', percentage)} ({percentage}%)");
+        }
+
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+
+    }
+}
---
+++ rollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2v2
+{
+    internal class rollDice
+    {
+        private static Random dice = new Random();
+
+
+        public static int[] RollingOfDice(int numRolls)
+        {
+            // This creates an array in which we will count the amount of times each number was rolled
+            int[] rollCount = [];
+            rollCount = new int[13];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                // This is going to generate our random number for each one of our rolls
+                int dice1 = dice.Next(1, 7);
+                int dice2 = dice.Next(1, 7);
+                int sumDice = dice1 + dice2;
+
+                rollCount[sumDice]++;
+            }
+
+            return rollCount;
+
+        }
+        }
+    }
+
