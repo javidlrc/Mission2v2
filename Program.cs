9b044fafa5fdc56a57285ebde45358d84a00870b
javierdlrc <javierdlrc@JAVIERDELOS6BBB>
2024-01-17 19:29:21 -0700

    added comments

--- Program.cs
+++ Program.cs
@@ -7,22 +7,26 @@ internal class Program
     {
         int numRolls = 0;
 
+        // starts the text of the program
         Console.WriteLine("Welcome to the dice throwing simulator!");
         Console.WriteLine("How many dice rolls would you like to simulate?");
 
+        // recieves user input and assigns it to numRolls variable
         numRolls = int.Parse(Console.ReadLine());
 
-        rollDice rd = new rollDice();
 
         Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         Console.WriteLine("Total number of rolls = {numRolls}.");
 
+        // assigns a new object to rollDice variable and assigns the array from the rollingofdice function to the summary array
         rollDice rollDice = new rollDice();
         int[] summary = rollDice.RollingOfDice(numRolls);
 
+        // loop to priint out the results and creates histogram
         for (int i = 2; i <= 12; i++) 
         { 
+            // goes over every position in the array and converts it into a percentage
             int percentage = summary[i] * 100 / numRolls;
             Console.WriteLine($"{i}: {new string('*', percentage)} ({percentage}%)");
         }
--- rollDice.cs
+++ rollDice.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace Mission2v2
 {
+    //new class for rollDice function
     internal class rollDice
     {
+        //grabs the random object to create random objects and stores it in the "dice" variable
         private static Random dice = new Random();
 
-
+        //rolling dice function
         public static int[] RollingOfDice(int numRolls)
         {
             // This creates an array in which we will count the amount of times each number was rolled
             int[] rollCount = [];
+            // this assigns 12 spots to the array
             rollCount = new int[13];
 
+            // loop so that it generates the total amount of scores (based on # of rolls) and it stores it in the array
             for (int i = 0; i < numRolls; i++)
             {
                 // This is going to generate our random number for each one of our rolls
@@ -24,9 +28,11 @@ namespace Mission2v2
                 int dice2 = dice.Next(1, 7);
                 int sumDice = dice1 + dice2;
 
+                //this is so that in every score (aka the position) it adds a point to it
                 rollCount[sumDice]++;
             }
 
+            // returns the array
             return rollCount;
 
         }
